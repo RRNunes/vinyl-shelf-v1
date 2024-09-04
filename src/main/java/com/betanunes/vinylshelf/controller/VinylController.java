b38027b801ae6c58ec0aead63ba9ed49dc994a15
Katyanne R. R. Nunes <betanunes8@gmail.com>
2024-09-04 19:08:30 -0400

    Fix query parameter

--- src/main/java/com/betanunes/vinylshelf/controller/VinylController.java
+++ src/main/java/com/betanunes/vinylshelf/controller/VinylController.java
@@ -2,21 +2,26 @@ package com.betanunes.vinylshelf.controller;
 
 import com.betanunes.vinylshelf.client.DiscogsClient;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VinylController {
+
     private static final int PAGE_SIZE = 20;
-    private final DiscogsClient discogsClient;
+
     public VinylController(DiscogsClient discogsClient) {
-        this.discogsClient = discogsClient;
+
     }
 
-    @SuppressWarnings("checkstyle:MagicNumber")
+    @SuppressWarnings({"checkstyle:MagicNumber", "checkstyle:WhitespaceAround"})
+
     @GetMapping("/search")
-    public ObjectNode search(@RequestParam("q") String query)  {
-        return discogsClient.search(query, "master", 1, PAGE_SIZE);
+    public ResponseEntity<ObjectNode> search(
+        @RequestParam("q") String query,
+        @RequestParam(value = "page", defaultValue = "1") int page) {
+        return null;
     }
 }
